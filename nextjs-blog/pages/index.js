1de71c6dc4fbe53cfa62e1e7d252ab19644b681f
LauJang <klaude.jang@gmail.com>
2023-04-03 14:16:15 +0900

    Import Layout component in index.js

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,25 +1,21 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import Layout, {siteTitle} from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Klaude Jang</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Klaude Jang
-        </h1>
+    <Layout home>
+    <Head>
+      <title>{siteTitle}</title>
+    </Head>
+    <section className={utilStyles.headingMd}>
+      <p>This is my personal blog, made with Next.js as a practice</p>
 
-        <p className={styles.description}>
-          This is my personal blog, made with Next.js as a practice
-        </p>
-
-        <div className={styles.grid}>
+    </section>
+    <div className={styles.grid}>
           <a href="https://github.com/LauJang" className={styles.card}>
             <h3>GitHub &rarr;</h3>
             <p>Please visit my Github to see what I'm currently working on!</p>
@@ -48,69 +44,6 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+  </Layout>
   )
 }
