18f21ab8c756572d1c0af33827e42916cb3196ee
LauJang <klaude.jang@gmail.com>
2023-04-04 16:53:21 +0900

    Toss posts data as props

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -4,7 +4,17 @@ import styles from '../styles/Home.module.css';
 import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts';
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({allPostsData}) {
   return (
 
     <Layout home>
