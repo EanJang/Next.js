4b5da20353746046a8a8ff18784eaf9074fdc7f0
LauJang <klaude.jang@gmail.com>
2023-04-05 11:27:51 +0900

    export async functions: getStaticPaths and getStaticProps

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -3,3 +3,11 @@ import Layout from '../../components/layout';
 export default function Post() {
     return <Layout>...</Layout>;
 }
+
+export async function getStaticPaths() {
+    // will return a list of possible values for id
+}
+
+export async function getStaticProps({params}) {
+    // will fetch necessary data for the post using params.id
+}
