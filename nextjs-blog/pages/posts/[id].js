386b1f2980d0973e136868f24b5766084e652478
LauJang <klaude.jang@gmail.com>
2023-04-06 10:51:38 +0900

    Install date-fns library to formate post date

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import utilStyles from '../../styles/utils.module.css';
 import Layout from '../../components/layout';
+import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export default function Post({ postData }) {
@@ -12,7 +13,7 @@ export default function Post({ postData }) {
         <article>
             <h1 className={utilStyles.headingXl}>{postData.title}</h1>
             <div className={utilStyles.lightText}>
-            {postData.date}
+            <Date dateString={postData.date} />
             </div>
             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </article>
